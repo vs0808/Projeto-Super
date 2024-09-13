621dd3cbc43d5c4b6f069b70548c82b80c72667f
vs0808 <vinicius.souza.menezes@gmail.com>
2024-09-13 13:47:06 -0300

    docs da tela de login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './index.css'
+import Login from './components/login/login';
+import BarraInferior from './components/barraInferior/barraInferior'
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <BarraInferior/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/barraInferior/barraInferior.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './barraInferior.css';
+
+const BarraInferior = () => {
+  return (
+    <div className="barra_inferior">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Quem somos</a></li>
+        <li><a href="#">Termos & Condições</a></li>
+        <li><a href="#">Política de Privacidade</a></li>
+        <li><a href="#">Fale Conosco</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default BarraInferior;
